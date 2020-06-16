cb3da5790b3bf79f7ae3817b2641a9e103d941d7
Dmytro Haidash <dmyto.haidash@gmail.com>
2020-06-16 09:28:08 +0300

    #

--- resources/views/partials/client/layout/header.blade.php
+++ resources/views/partials/client/layout/header.blade.php
@@ -1,7 +1,7 @@
 <header class="app-header">
     <div class="logo self-start h-full">
         <a href="{{ url('/') }}" class="block h-full">
-            <svg fill="#fff" width="240" height="60">
+            <svg fill="#fff" width="300" height="60">
                 <use xlink:href="{{app()->getLocale() == 'ru' ? '#logo_rus' : '#logo'}}"></use>
             </svg>
         </a>
