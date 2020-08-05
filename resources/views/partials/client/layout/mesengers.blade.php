49900864bebf37439a7564052b4beece8e3372c9
Dmytro Haidash <dmyto.haidash@gmail.com>
2020-08-05 18:58:11 +0300

    bug fix

--- resources/views/partials/client/layout/mesengers.blade.php
+++ resources/views/partials/client/layout/mesengers.blade.php
@@ -1,12 +1,12 @@
 <ul class="messengers list-unstyled">
     <li>
-        <a href="https://www.facebook.com/" target="_blank">
+        <a href="https://www.facebook.com/Caucasian-Arms-Center-of-research-%D0%98%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5-%D0%9E%D1%80%D1%83%D0%B6%D0%B8%D0%B5-%D0%9A%D0%B0%D0%B2%D0%BA%D0%B0%D0%B7%D0%B0-106325201183387/" target="_blank">
             <svg width="20" height="20">
                 <use xlink:href="#fb"></use>
             </svg>
         </a>
     </li>
-    <li>
+    {{--<li>
         <a href="https://www.instagram.com/"  target="_blank">
             <svg width="20" height="20">
                 <use xlink:href="#insta"></use>
@@ -19,5 +19,5 @@
                 <use xlink:href="#youtube"></use>
             </svg>
         </a>
-    </li>
+    </li>--}}
 </ul>
